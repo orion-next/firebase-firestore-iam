a470ccb33c9129ce40262a89a4321ec22ff0c306
Vidush H. Namah <vidush.namah@orion-next.com>
2026-03-27 13:51:11 +0400

    Add error handling + transactions on multiple doc writes - Restructured services to make logical sense - Some type changes (non-breaking) - Cleaner log event function call

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions/v1";
 import * as admin from "firebase-admin";
 
 import { AccountDocumentType, Collections, EventAction, EventSource } from "./types";
-import { LogEvent } from "./utilities";
+import { HandleError, LogEvent } from "./utilities";
 import { AccountService, AuthenticationService } from "./services";
 
 admin.initializeApp();
@@ -12,45 +12,94 @@ const auth = admin.auth();
 
 export const SyncAccountOnUserCreated = functions.auth.user().onCreate(async (user) => {
     if (!user.email) return;
+    const user_email = user.email;
 
-    const email = user.email;
-    const account_reference = db.collection(Collections.Accounts).doc(email);
-    const account_document = await account_reference.get();
+    try {
+        const account_reference = db.collection(Collections.Accounts).doc(user_email);
+        const account_document = await account_reference.get();
 
-    if (account_document.exists) {
-        const account_data = account_document.data() as AccountDocumentType;
-        await AccountService.SyncWithAuthenticationUser(auth, email, account_data);
-        await LogEvent(db, email, EventAction.USER_CREATED, EventSource.AUTH_CHANGE_TRIGGER);
-    } else {
-        await account_reference.set({});
-        functions.logger.info(`Created empty account document for ${email}.`);
-        await LogEvent(db, email, EventAction.DOCUMENT_CREATED, EventSource.AUTH_CHANGE_TRIGGER);
+        if (account_document.exists) {
+            const account_data = account_document.data() as AccountDocumentType;
+            await AuthenticationService.SyncUserFromDocument(auth, user_email, account_data);
+            await LogEvent(db, user_email, {
+                action: EventAction.USER_CREATED,
+                source: EventSource.AUTH_CHANGE_TRIGGER
+            });
+        } else {
+            await db.runTransaction(async (tx) => {
+                tx.set(account_reference, {});
+                functions.logger.info(`Created empty account document for ${user_email}.`);
+                await LogEvent(db, user_email, {
+                    action: EventAction.DOCUMENT_CREATED, 
+                    source: EventSource.AUTH_CHANGE_TRIGGER
+                }, tx);
+            });
+        }
+    } catch (err) {
+        HandleError(err, SyncAccountOnUserCreated.name);
     }
 });
 
 export const DeleteAccountOnUserDeleted = functions.auth.user().onDelete(async (user) => {
     if (!user.email) return;
-    await AccountService.SoftDeleteDocument(db, user.email);
-    await LogEvent(db, user.email, EventAction.USER_DELETED, EventSource.AUTH_CHANGE_TRIGGER);
+    var user_email = user.email;
+    try {
+        await db.runTransaction(async (tx) => {
+            await AccountService.SoftDeleteDocument(tx, db, user_email);
+            await LogEvent(db, user_email, {
+                action: EventAction.USER_DELETED,
+                source: EventSource.AUTH_CHANGE_TRIGGER
+            }, tx);
+        });
+    } catch (err) {
+        HandleError(err, DeleteAccountOnUserDeleted.name);
+    }
 });
 
-export const SyncUserOnAccountCreated = functions.firestore.document("Accounts/{email}").onCreate(async (change, context) => {
-    const email = context.params.email;
+export const SyncUserOnAccountCreated = functions.firestore.document(`${Collections.Accounts}/{email}`).onCreate(async (change, context) => {
+    const user_email = context.params.email;
     const account_data = change.data() as AccountDocumentType;
-    await AccountService.SyncWithAuthenticationUser(auth, email, account_data);
-    await LogEvent(db, email, EventAction.USER_UPDATED, EventSource.DOC_CHANGE_TRIGGER);
+
+    try {
+        await AuthenticationService.SyncUserFromDocument(auth, user_email, account_data);
+        await LogEvent(db, user_email, {
+            action: EventAction.USER_UPDATED,
+            source: EventSource.DOC_CHANGE_TRIGGER
+        });
+    } catch (err) {
+        HandleError(err, SyncUserOnAccountCreated.name);
+    }
 });
 
-export const SyncUserOnAccountUpdated = functions.firestore.document("Accounts/{email}").onUpdate(async (change, context) => {
-    const email = context.params.email;
+export const SyncUserOnAccountUpdated = functions.firestore.document(`${Collections.Accounts}/{email}`).onUpdate(async (change, context) => {
+    const user_email = context.params.email;
     const account_data = change.after.data() as AccountDocumentType;
-    await AccountService.SyncWithAuthenticationUser(auth, email, account_data);
-    await LogEvent(db, email, EventAction.USER_UPDATED, EventSource.DOC_CHANGE_TRIGGER);
+
+    try {
+        await AuthenticationService.SyncUserFromDocument(auth, user_email, account_data);
+        await LogEvent(db, user_email, {
+            action: EventAction.USER_UPDATED,
+            source: EventSource.DOC_CHANGE_TRIGGER
+        });
+    } catch (err) {
+        HandleError(err, SyncUserOnAccountUpdated.name);
+    }
 });
 
 export const DeleteUserOnAccountDeleted = functions.firestore.document(`${Collections.Accounts}/{email}`).onDelete(async (_snap, context) => {
-    const email = context.params.email;
-    await AuthenticationService.DeleteUserAndRevokeToken(auth, email);
-    await AccountService.SoftDeleteDocument(db, email);
-    await LogEvent(db, email, EventAction.USER_DELETED, EventSource.DOC_CHANGE_TRIGGER);
+    const user_email = context.params.email;
+
+    try {
+        await AuthenticationService.DeleteUserAndRevokeToken(auth, user_email);
+
+        await db.runTransaction(async (tx) => {
+            await AccountService.SoftDeleteDocument(tx, db, user_email);
+            await LogEvent(db, user_email, {
+                action: EventAction.USER_DELETED,
+                source: EventSource.DOC_CHANGE_TRIGGER
+            }, tx);
+        });
+    } catch (err) {
+        HandleError(err, DeleteUserOnAccountDeleted.name);
+    }
 });
--- functions/src/utilities.ts
+++ functions/src/utilities.ts
@@ -1,22 +1,47 @@
-import { FieldValue, Firestore } from "firebase-admin/firestore";
-import { Collections, EventLogType } from "./types";
+import * as functions from "firebase-functions";
+import { FieldValue, Firestore, Transaction } from "firebase-admin/firestore";
+import { Collections, ErrorCodes, EventLogType, Actors } from "./types";
+import { FirebaseError } from "firebase-admin";
 
-const SYSTEM_ACTOR = "system";
+export const LogEvent = async (db: Firestore, email: string, event: Omit<EventLogType, "timestamp" | "actor">, tx?: Transaction) => {
+    const logRef = db
+        .collection(Collections.Accounts).doc(email)
+        .collection(Collections.EventLogs).doc();
 
-export const LogEvent = async (db: Firestore, email: string, action: string, source: string, details?: Record<string, any>) => {
-  const logRef = db
-    .collection(Collections.Accounts)
-    .doc(email)
-    .collection(Collections.EventLogs)
-    .doc();
+    var eventLog: EventLogType = {
+        timestamp: FieldValue.serverTimestamp(),
+        actor: Actors.SYSTEM,    
+        ...event
+    };
 
-  var log : EventLogType = {
-    timestamp: FieldValue.serverTimestamp(),
-    actor: SYSTEM_ACTOR,
-    action,
-    source
-  };
-
-  if (details) log.details = details;
-  await logRef.set(log);
+    tx ? tx.set(logRef, eventLog) : await logRef.set(eventLog);
 }
+
+export const HandleError = (error: unknown, source: string) => {
+    if (GuardForFirebaseError(error)) {
+        switch (error.code) {
+            case ErrorCodes.AUTH_USER_NOT_FOUND:
+                functions.logger.warn(
+                    `[${source}] User not found.`
+                );
+                break;
+
+            case ErrorCodes.AUTH_INVALID_EMAIL:
+                functions.logger.error(
+                    `[${source}] Invalid email.`
+                );
+                break;
+
+            default:
+                functions.logger.error(
+                    `[${source}] Unexpected error ${error.message}.`,
+                    error
+                );
+                break;
+        }
+    }
+
+    functions.logger.error(`[${source}] Unexpected error.`, error);
+}
+
+const GuardForFirebaseError = (err: unknown) : err is FirebaseError => typeof err === "object" && err !== null && "code" in err;
