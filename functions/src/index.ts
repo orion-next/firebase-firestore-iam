3dba7b56928ef33dfb445e8fd9b986e57663babe
Vidush H. Namah <vidush.namah@orion-next.com>
2026-03-29 14:56:38 +0400

    Use uid as document reference + Parameterise all creates, updates and deletes for user and doc

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions/v1";
 import * as admin from "firebase-admin";
 
-import { AccountDocumentType, Collections, EventAction, EventSource } from "./types";
+import { AccountDocumentType, Collections, ENV, EventAction, EventSource } from "./types";
 import { HandleError, LogEvent } from "./utilities";
 import { AccountService, AuthenticationService } from "./services";
 
@@ -11,25 +11,25 @@ const db = admin.firestore();
 const auth = admin.auth();
 
 export const SyncAccountOnUserCreated = functions.auth.user().onCreate(async (user) => {
-    if (!user.email) return;
-    const user_email = user.email;
+    if (!user.uid) return;
+    const user_uid = user.uid;
 
     try {
-        const account_reference = db.collection(Collections.Accounts).doc(user_email);
+        const account_reference = db.collection(Collections.Accounts).doc(user_uid);
         const account_document = await account_reference.get();
 
         if (account_document.exists) {
             const account_data = account_document.data() as AccountDocumentType;
-            await AuthenticationService.SyncUserFromDocument(auth, user_email, account_data);
-            await LogEvent(db, user_email, {
+            await AuthenticationService.SyncUserFromDocument(auth, user_uid, account_data);
+            await LogEvent(db, user_uid, {
                 action: EventAction.USER_CREATED,
                 source: EventSource.AUTH_CHANGE_TRIGGER
             });
-        } else {
+        } else if (ENV.CREATE_DOC_ON_USER_CREATED.value()) {
             await db.runTransaction(async (tx) => {
                 tx.set(account_reference, {});
-                functions.logger.info(`Created empty account document for ${user_email}.`);
-                await LogEvent(db, user_email, {
+                functions.logger.info(`Created empty account document for ${user_uid}.`);
+                await LogEvent(db, user_uid, {
                     action: EventAction.DOCUMENT_CREATED,
                     source: EventSource.AUTH_CHANGE_TRIGGER
                 }, tx);
@@ -41,12 +41,14 @@ export const SyncAccountOnUserCreated = functions.auth.user().onCreate(async (us
 });
 
 export const DeleteAccountOnUserDeleted = functions.auth.user().onDelete(async (user) => {
-    if (!user.email) return;
-    var user_email = user.email;
+    if (!ENV.DELETE_DOC_ON_USER_DELETED.value()) return;
+
+    if (!user.uid) return;
+    var user_uid = user.uid;
     try {
         await db.runTransaction(async (tx) => {
-            await AccountService.SoftDeleteDocument(tx, db, user_email);
-            await LogEvent(db, user_email, {
+            await AccountService.SoftDeleteDocument(tx, db, user_uid);
+            await LogEvent(db, user_uid, {
                 action: EventAction.USER_DELETED,
                 source: EventSource.AUTH_CHANGE_TRIGGER
             }, tx);
@@ -56,13 +58,13 @@ export const DeleteAccountOnUserDeleted = functions.auth.user().onDelete(async (
     }
 });
 
-export const SyncUserOnAccountCreated = functions.firestore.document(`${Collections.Accounts}/{email}`).onCreate(async (change, context) => {
-    const user_email = context.params.email;
+export const SyncUserOnAccountCreated = functions.firestore.document(`${Collections.Accounts}/{uid}`).onCreate(async (change, context) => {
+    const user_uid = context.params.uid;
     const account_data = change.data() as AccountDocumentType;
 
     try {
-        await AuthenticationService.SyncUserFromDocument(auth, user_email, account_data);
-        await LogEvent(db, user_email, {
+        await AuthenticationService.SyncUserFromDocument(auth, user_uid, account_data);
+        await LogEvent(db, user_uid, {
             action: EventAction.USER_UPDATED,
             source: EventSource.DOC_CHANGE_TRIGGER
         });
@@ -71,13 +73,13 @@ export const SyncUserOnAccountCreated = functions.firestore.document(`${Collecti
     }
 });
 
-export const SyncUserOnAccountUpdated = functions.firestore.document(`${Collections.Accounts}/{email}`).onUpdate(async (change, context) => {
-    const user_email = context.params.email;
+export const SyncUserOnAccountUpdated = functions.firestore.document(`${Collections.Accounts}/{uid}`).onUpdate(async (change, context) => {
+    const user_uid = context.params.uid;
     const account_data = change.after.data() as AccountDocumentType;
 
     try {
-        await AuthenticationService.SyncUserFromDocument(auth, user_email, account_data);
-        await LogEvent(db, user_email, {
+        await AuthenticationService.SyncUserFromDocument(auth, user_uid, account_data);
+        await LogEvent(db, user_uid, {
             action: EventAction.USER_UPDATED,
             source: EventSource.DOC_CHANGE_TRIGGER
         });
@@ -86,15 +88,17 @@ export const SyncUserOnAccountUpdated = functions.firestore.document(`${Collecti
     }
 });
 
-export const DeleteUserOnAccountDeleted = functions.firestore.document(`${Collections.Accounts}/{email}`).onDelete(async (_snap, context) => {
-    const user_email = context.params.email;
+export const DeleteUserOnAccountDeleted = functions.firestore.document(`${Collections.Accounts}/{uid}`).onDelete(async (_snap, context) => {
+    if (!ENV.DELETE_USER_ON_DOC_DELETED.value()) return;
+    
+    const user_uid = context.params.uid;
 
     try {
-        await AuthenticationService.DeleteUserAndRevokeToken(auth, user_email);
+        await AuthenticationService.DeleteUserAndRevokeToken(auth, user_uid);
 
         await db.runTransaction(async (tx) => {
-            await AccountService.SoftDeleteDocument(tx, db, user_email);
-            await LogEvent(db, user_email, {
+            await AccountService.SoftDeleteDocument(tx, db, user_uid);
+            await LogEvent(db, user_uid, {
                 action: EventAction.USER_DELETED,
                 source: EventSource.DOC_CHANGE_TRIGGER
             }, tx);
